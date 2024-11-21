155973fff55cd247bdad2ddd8ca038e243b2f8fc
Sahil Kapadia <sahilkapadia@sahils-mbp.wireless-1x.unc.edu>
2024-11-21 15:33:23 -0500

    Final changes

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
 # builtin
-import os
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
 # external
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from pinecone import Pinecone
 
 # internal
 from utils import produce_context
@@ -17,11 +16,18 @@ from openai_methods import (
 )
 from config import PINECONE_API_KEY
 from models import EntityMetadata
+from pinecone import Pinecone
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
-index = pc.Index('relationships-index')
-
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up")
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    index = pc.Index('relationships-index')
+    app.state.pc = pc
+    app.state.index = index
+    yield
+    print("Shutting down")
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,30 +46,32 @@ class QuestionResponse(BaseModel):
     context: list[str]
 
 @app.post("/ask", response_model=QuestionResponse)
-async def ask_question(request: QuestionRequest):
-    user_question: str = request.question.strip()
-    num_matches: int = request.num_matches
+async def ask_question(request: Request, question_request: QuestionRequest):
+    app = request.app
+    user_question: str = question_request.question.strip()
+    num_matches: int = question_request.num_matches
 
     if not user_question:
         raise HTTPException(status_code=400, detail="Please enter a valid question.")
 
     relationship_type: str = extract_relationship_type(question=user_question)
-    
     relationship_embedding: list[float] = create_embedding(text=relationship_type)
-    
-    results = query_database(k_num=num_matches, relationship_embedding=relationship_embedding)
+
+    index = app.state.index
+    results = query_database(
+        k_num=num_matches, relationship_embedding=relationship_embedding, index=index
+    )
 
     entity_dict: dict[int, EntityMetadata] = {}
     for i, match in enumerate(results['matches']):
         entity = EntityMetadata(
-            entity1_id=match['metadata'].get('entity1_id'), 
-            entity2_id=match['metadata'].get('entity2_id'), 
+            entity1_id=match['metadata'].get('entity1_id'),
+            entity2_id=match['metadata'].get('entity2_id'),
             relationship=match['metadata'].get('relationship_type')
         )
         entity_dict[i] = entity
 
     context: list[str] = produce_context(entity_dict=entity_dict)
-    
     answer: str = user_output_generation(question=user_question, context=context)
 
     response = QuestionResponse(
